43b8f8ac907a955df2e4d7f8cda3009f0488c12e
Praveen Jagarlamudi <jagarlamudi51@gmail.com>
2019-09-07 14:34:45 -0500

    Add Configuration class

---
+++ APITest/ConfigureServices.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace APITest
+{
+    class ConfigureServices
+    {
+        public static IServiceCollection CreateServices()
+        {
+            var services = new ServiceCollection();
+
+            // Configure Settings
+            services.AddOptions();
+            
+            // Add Dependencies
+
+            // NOTE: This line is essential so that Microsoft.Extensions.DependencyInjection knows
+            // about the SpecFlow bindings (something normally BoDi does automatically).
+            foreach (var type in typeof(ConfigureServices).Assembly.GetTypes().Where(t => Attribute.IsDefined(t, typeof(BindingAttribute))))
+            {
+                services.AddSingleton(type);
+            }
+
+            return services;
+        }
+    }
+}
---
+++ Dosakaya.API.Common/Dosakaya.API.Common/Configurations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace Dosakaya.API.Common
+{
+    class Configurations
+    {
+        public static IConfigurationRoot Config { get; private set; }
+
+        public static void Init()
+        {
+            var env = Environment.GetEnvironmentVariable("TEST_ENVIRONMENT") ?? "DEV";
+            Config = Config ?? new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddYamlFile("Settings.yaml")
+                .AddYamlFile($"Settings.{env}.yml")
+                .Build();                
+        }
+    }
+}
